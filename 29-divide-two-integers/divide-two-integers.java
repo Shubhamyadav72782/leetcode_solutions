b5f2351b979dc3545e2c0b35ada4778ab5bbdbe5
SHUBHAM YADAV <yadavshubham72782@gmail.com>
2026-07-04 16:33:13 +0530

    Time: 2 ms (2.94%) | Memory: 42.7 MB (31.57%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int divide(int dividend, int divisor) {
 
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
@@ -25,6 +25,57 @@ class Solution {
             return -result;
         }
 
+        return result;
+    }
+}*/
+
+
+
+
+
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        long left = 0;
+        long right = dvd;
+        long ans = 0;
+
+        while(left <= right){
+            long mid = left + (right - left)/2;
+            
+            long product = multiply(mid, dvs);
+
+            if(product <= dvd){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+
+            if((dividend < 0) ^ (divisor < 0)){
+                ans = -ans;
+            }
+        
+        return (int) ans;
+
+    }
+
+    public long multiply(long a, long b){
+        long result = 0;
+        while(b >0){
+        if((b & 1) == 1){
+            result += a;
+        }
+        a <<= 1;
+        b >>= 1;
+        }
         return result;
     }
 }
