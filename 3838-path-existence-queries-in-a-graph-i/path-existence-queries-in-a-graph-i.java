90226078887e5725c9e3a3950b55439e0b2cb141
SHUBHAM YADAV <yadavshubham72782@gmail.com>
2026-07-09 11:09:39 +0530

    Time: 10 ms (44.55%) | Memory: 163 MB (68.32%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
         int[] component = new int[n];
         int id = 0;
@@ -16,5 +16,55 @@ class Solution {
         }
 
         return ans;
+    }
+}*/
+
+
+
+class UnionFind {
+    int[] parent;
+    UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find(int a) {
+        if (parent[a] != a) {
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
+    }
+
+    boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA == rootB) {
+            return false;
+        }
+        parent[rootA] = rootB;
+        return true;
+    }
+}
+
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        UnionFind uf = new UnionFind(n);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                uf.union(i, i - 1);
+            }
+        }
+
+        boolean[] res = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            if (uf.find(u) == uf.find(v)) {
+                res[i] = true;
+            }
+        }
+        return res;
     }
 }
