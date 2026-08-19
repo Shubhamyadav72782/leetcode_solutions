b52fd52ec4ca7744077fc84e02f32b6d3bcf9641
SHUBHAM YADAV <yadavshubham72782@gmail.com>
2026-08-19 21:33:09 +0530

    Time: 27 ms (44.21%) | Memory: 52.3 MB (65.53%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            int row = seat[0];
+            int col = seat[1];
+
+            map.computeIfAbsent(row, k -> new HashSet<>()).add(col);
+        }
+
+        int ans = (n - map.size()) * 2;
+
+        for (Set<Integer> seats : map.values()) {
+
+            boolean left = true;
+            boolean right = true;
+            boolean middle = true;
+
+            // Left: 2,3,4,5
+            for (int i = 2; i <= 5; i++) {
+                if (seats.contains(i)) {
+                    left = false;
+                    break;
+                }
+            }
+
+            // Right: 6,7,8,9
+            for (int i = 6; i <= 9; i++) {
+                if (seats.contains(i)) {
+                    right = false;
+                    break;
+                }
+            }
+
+            // Middle: 4,5,6,7
+            for (int i = 4; i <= 7; i++) {
+                if (seats.contains(i)) {
+                    middle = false;
+                    break;
+                }
+            }
+
+            if (left && right) {
+                ans += 2;
+            } else if (left || right || middle) {
+                ans += 1;
+            }
+        }
+
+        return ans;
+    }
+}
