731537aff4ed00c245cc9d4e8df1119079738dcf
SHUBHAM YADAV <yadavshubham72782@gmail.com>
2026-08-23 15:17:49 +0530

    Time: 8 ms (53.28%) | Memory: 47 MB (64.23%) - LeetSync

---
+++ 2039-sum-game/sum-game.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        int n = num.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                leftQ++;
+            } else {
+                leftSum += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                rightQ++;
+            } else {
+                rightSum += num.charAt(i) - '0';
+            }
+        }
+
+        // If number of ? is odd, Alice always wins
+        if ((leftQ + rightQ) % 2 == 1) {
+            return true;
+        }
+
+        // Difference between the two halves
+        int diff = leftSum - rightSum;
+
+        // Difference in number of ? on both sides
+        int qDiff = leftQ - rightQ;
+
+        return diff + (qDiff / 2) * 9 != 0;
+    }
+}
